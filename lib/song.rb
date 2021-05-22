f92c184f56fbe4f45998ab436ce18f1a75a3cc10
Oscar Alvarez <gavinalv6@gmail.com>
2021-05-22 16:03:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   end
 
   def self.find_by_name(name)
-    self.all.find{|song| song.name == name}
+    self.all.detect{|s| s.name == name}
   end
 
   def self.find_or_create_by_name(song_name)
