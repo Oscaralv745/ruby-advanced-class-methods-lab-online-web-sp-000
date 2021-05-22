d45b0c03aa1f5594423a6921f2d55eb85b200d3e
Oscar Alvarez <gavinalv6@gmail.com>
2021-05-22 15:52:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,46 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = Song.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name
+
+    @@all << self
+  end
+
+  def self.find_by_name
+
+  end
+
+  def self.find_or_create_by_name
+
+  end
+
+  def self.alphabetical
+
+  end
+
+  def self.new_from_filename
+
+  end
+
+  def self.create_from_filename
+
+  end
+
+  def self.destroy_all
+
+  end
+
+
 end
